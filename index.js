04ed308282ded767034564913eb06407c3102e7e
Tony Metzidis <anthony.metzidis@gmail.com>
2017-03-18 16:11:39 -0700

    promises and better tests

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
 var AWS = require('aws-sdk')
   , debug = require('debug')('knox-ec2-role')
   , knox = require('knox')
-  , Promise = require('bluebird')
 
 module.exports = {
   authenticate: function (conf, httpOptions) {
-    return new Promise(function (resolve, reject) {
-      if (!conf) {
-        conf = {}
-      }
-      Object.assign(AWS.config.httpOptions, httpOptions)
-      var metadata = new AWS.MetadataService()
-      debug('loading credentials from aws metadata')
-      metadata.loadCredentials(function (err, creds) {
-        if (err) {
-          debug('error: ', err)
-          reject(err)
-          return
-        }
+    conf = conf || {}
+    var metadata = new AWS.MetadataService()
+    Object.assign(AWS.config.httpOptions, httpOptions)
+    return metadata.loadCredentials().promise()
+      .then(function (creds) {
+        debug('loading credentials from aws metadata')
         debug(creds)
         Object.assign(conf, {key: creds.AccessKeyId, secret: creds.SecretAccessKey, token: creds.Token})
         try {
           var client = knox.createClient(conf)
-          resolve(client)
+          return Promise.resolve(client)
         } catch (e) {
-          debug(e)
-          debug(e.stack)
-          reject(e)
+          return Promise.reject(e)
         }
       })
-    })
   }
 }
