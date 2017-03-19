a62298982aa2c7f71dad0a4cf2bbf55d47ad06f6
Tony Metzidis <tonym@tonym.us>
2017-03-18 17:46:04 -0700

    use AWS promises and better tests
    * promises and better tests
    
    * fix
    
    * move chai
    
    * debug

--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,25 @@
+'use strict'
 var chai = require('chai')
 , expect = chai.expect
 , proxyquire = require('proxyquire')
 , AWS = require('aws-sdk-mock')
-, knoxec2
+chai.use(require('chai-as-promised'))
 
-AWS.mock('MetadataService', 'loadCredentials', function (callback) {
-  callback(null, {AccessKeyId: process.env.ACCESS_KEY_ID
-    , SecretAccessKey: process.env.SECRET_ACCESS_KEY})
-})
-knoxec2 = proxyquire('../index.js', { 'AWS': AWS })
-
-describe('knox-ec2-role', function () {
+describe('uploading files', function () {
+  let knoxec2
+  before(function () {
+    AWS.mock('MetadataService', 'loadCredentials', function (callback) {
+      callback(null, {AccessKeyId: process.env.ACCESS_KEY_ID
+        , SecretAccessKey: process.env.SECRET_ACCESS_KEY})
+    })
+    knoxec2 = proxyquire('../index.js', { 'AWS': AWS })
+  })
+  after(function () {
+    knoxec2 = null
+    AWS.restore('MetadataService')
+  })
   it('should upload file', function (done) {
-    var defaultConf = {bucket: process.env.K2_BUCKET}
-    knoxec2.authenticate(defaultConf, {timeout: 5000})
+    knoxec2.authenticate({bucket: process.env.K2_BUCKET}, {timeout: 5000})
       .then(function (client) {
         var object = { foo: 'bar' }
         , string = JSON.stringify(object)
@@ -27,9 +33,39 @@ describe('knox-ec2-role', function () {
         })
         req.end(string)
       })
-      .catch(function (e) {
-        expect(e).to.not.be.true
-        done()
+  })
+  it('should fail to upload with a bad bucket', function (done) {
+    knoxec2.authenticate({bucket: process.env.K2_BUCKET + 'xxx'}, {timeout: 5000})
+      .then(function (client) {
+        var object = { foo: 'bar' }
+        , string = JSON.stringify(object)
+        , req = client.put('/test/obj.json', {
+            'Content-Length': Buffer.byteLength(string)
+          , 'Content-Type': 'application/json'
+        })
+        req.on('response', function (res) {
+          expect(res.statusCode).to.equal(404)
+          done()
+        })
+        req.end(string)
       })
+  })
+})
+
+describe('failed metadata test', function () {
+  let knoxec2
+  before(function () {
+    AWS.mock('MetadataService', 'loadCredentials', function (callback) {
+      // force error
+      callback(true)
+    })
+    knoxec2 = proxyquire('../index.js', { 'AWS': AWS })
+  })
+  after(function () {
+    knoxec2 = null
+    AWS.restore('MetadataService')
+  })
+  it('should reject a broken promise', function () {
+    return expect(knoxec2.authenticate({bucket: process.env.K2_BUCKET}, {timeout: 5000})).to.be.rejected
   })
 })
